163edfafe769fa58ba466de39595b5abed0475cd
tien-nguyen-95 <quangtiennd95@gmail.com>
2020-02-03 16:27:32 +0700

    th4

---
+++ tong_quan_PHP/thuc-hanh/tu-dien/dictionary.php
@@ -0,0 +1,23 @@
+<?php
+$dictionary = array(
+    "hello"=>"xin chào", 
+    "how"=>"thế nào", 
+    "book"=>"quyển vở", 
+    "computer"=>"máy tính");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $searchword = $_POST["search"]; 
+    $flag = 0;
+    foreach($dictionary as $word => $description) {
+        if($word == $searchword){
+            echo "Từ: " . $word . ". <br/>Nghĩa của từ: " . $description;
+            echo "<br/>";
+            $flag = 1;
+        }
+    } 
+
+    if($flag == 0){
+        echo "Không tìm thấy từ cần tra.";
+    }
+}
+?>
